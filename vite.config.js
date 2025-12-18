c32ce93a11a43fbd6077c65b4716e30396a8f7fb
diyanair1 <diyaanoop@example.com>
2025-12-18 14:02:51 -0500

    Add cache busting to vite build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: { // "Cache busting" to load newest version of website without using old cached stuff
+    rollupOptions: {
+      output: {
+        // Force new file names on each build by adding different hashes
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 })
